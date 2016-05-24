03bab1476c229171d8f4e350e462d5a77fd36906
jordyr <Thnkvsn741>
2016-05-24 19:38:08 +0200

    Removal

--- core/target/test-classes/drop_tables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE league;
-DROP TABLE season;
-DROP TABLE team;
-DROP TABLE team_season;
-DROP TABLE player;
-DROP TABLE league_match;
-DROP TABLE player_stats;
--- core/target/test-classes/update.sql
+++
@@ -1,2 +0,0 @@
-update league_match set playerStatsUpdated = 0 where id = 2008;
-delete from player_stats where matchid = 2008;
